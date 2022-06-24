fc89f44ce492172aa8ab727dd2723ec524257237
tayaym <ty012563@gmail.com>
2022-06-24 18:41:26 +0900

    devise、モデル、viewsなどの導入と設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_index, except: [:index, :show]
   before_action :basic_auth
 
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
