109ffd0ab5632cdde007e57fae7e15488656cff2
tayaym <ty012563@gmail.com>
2022-07-05 12:52:02 +0900

    商品出品機能の実装の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,22 @@
 class Item < ApplicationRecord
 
-
   belongs_to :user
   # has_one :purchase
   has_one_attached :image
 
   validates :name, presence: true 
-  validates :profile, presence: true, unless: :was_attached?
+  validates :profile, presence: true
   validates :category_id, presence: true
   validates :state_id, presence: true
   validates :delivery_id, presence: true
   validates :region_id, presence: true
   validates :time_count_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
-  # validates :content, presence: true, unless: :was_attached?
 
-  def was_attached?
-    self.image.attached?
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery
+  belongs_to :region
+  belongs_to :time_count
 end
