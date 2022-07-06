b16fce142a01301ca7af3b300bf933341701d91b
tayaym <ty012563@gmail.com>
2022-07-06 17:34:48 +0900

    商品出品機能の修正

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -7,8 +7,9 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
    priceInput.addEventListener("input", () => {
      const addTaxDom = document.getElementById("add-tax-price");
-     addTaxDom.innerHTML = Math.floor(Input.value * 0.1 );
+     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
      const addProfitDom = document.getElementById("profit");
-     addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML)
+     addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
+
  })
 });
